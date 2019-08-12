8ff9eefa6f7681273d1d2c126d5fc2fb1fb0e16e
Fiftyppee <54045033+Fiftyppee@users.noreply.github.com>
2019-08-12 16:55:41 -0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,39 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
+    if message.content.startswith("こんばんは"):
+        if client.user != message.author:
+            m="こんばんは" + message.author.name + "さん！"
+            await message.channel.send(m)
+    if message.content.startswith("こんにちは"):
+        if client.user != message.author:
+            m="こんにちは" + message.author.name + "さん！"
+            await message.channel.send(m)
+    if message.content.startswith("雑魚"):
+        if client.user != message.author:
+            m="お前が雑魚だよ" + message.author.name + "さん！"
+            await message.channel.send(m)
+    if message.content.startswith("死ね"):
+        if client.user != message.author:
+            m="お前が死ねよ" + message.author.name + "さん！"
+            await message.channel.send(m)
+    
+client.run("NjEwMjAxODI2NDczNjcyNzYz.XVHa0Q.ON4OZxQzBpX2GEl8ViBO-bvmeC4")
